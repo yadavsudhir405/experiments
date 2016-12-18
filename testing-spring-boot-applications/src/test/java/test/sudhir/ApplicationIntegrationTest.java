3985b825dbfb0457a07dc0b9fb115821c4f94e42
Sudhir.Yadav <Sudhir.Yadav@mu-sigma.com>
2016-12-18 16:48:41 +0530

    No need of @Value property LocalserverPort annotation is added that takes care of that

--- testing-spring-boot-applications/src/test/java/test/sudhir/ApplicationIntegrationTest.java
+++ testing-spring-boot-applications/src/test/java/test/sudhir/ApplicationIntegrationTest.java
@@ -3,7 +3,7 @@ package test.sudhir;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationIntegrationTest {
 
-	@Value("${local.server.port}")
+	@LocalServerPort
 	private int port;
 
 	private TestRestTemplate restTemplate=new TestRestTemplate();
