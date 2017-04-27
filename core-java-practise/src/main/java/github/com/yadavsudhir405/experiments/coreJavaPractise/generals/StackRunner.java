fd5f20f32df9ab91f78b3f61da0ceee81b2c7de0
Sudhir.Yadav <Sudhir.Yadav@mu-sigma.com>
2017-04-27 09:33:20 +0530

    Stack inifinfite push

---
+++ core-java-practise/src/main/java/github/com/yadavsudhir405/experiments/coreJavaPractise/generals/StackRunner.java
@@ -0,0 +1,19 @@
+package github.com.yadavsudhir405.experiments.coreJavaPractise.generals;
+
+import java.util.Stack;
+
+/**
+ * @author sudhir
+ *         Date:24/4/17
+ *         Time:10:33 PM
+ *         Project:core-java-practise
+ */
+public class StackRunner {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        boolean isTrue = true;
+        while (isTrue) {
+            stack.push(10);
+        }
+    }
+}
